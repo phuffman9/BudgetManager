e12868ff3fc0d5e7c5db56e90c76fced914bc910
Peyton Huffman <109002808+phuffman9@users.noreply.github.com>
2024-09-06 08:54:37 -0400

    Add files via upload

---
+++ Budget.h
@@ -0,0 +1,8 @@
+#pragma once
+class Budget{
+    public:
+
+    private:
+    double totalBudget;
+    double budgetRemaining;
+};
---
+++ main.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+#include <cstdlib>
+using namespace std;
+
+int main(){
+    cout << "Welcome to the monthly budget manager!" << endl;
+    return EXIT_SUCCESS;
+}
