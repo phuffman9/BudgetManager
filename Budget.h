2033da5085312df92ba7d3759776d483ff22edc2
Peyton Huffman <109002808+phuffman9@users.noreply.github.com>
2024-09-13 19:26:01 -0400

    Delete Budget.h

--- Budget.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-class Budget{
-    public:
-
-    private:
-    std::vector<std::vector<std::tuple<std::string, int>>> purchases(12);
-    double totalBudget;
-    double budgetRemaining;
-};
